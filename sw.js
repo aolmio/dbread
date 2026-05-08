f7458a914099e1be41b29ac30e07d686ebe4502e
aolmio <mchan448@gmail.com>
2026-05-08 23:56:32 +0630

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,21 +1,10 @@
-const CACHE_NAME = 'gold-pro-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
-];
+const CACHE_NAME = 'gold-pro-v3';
+const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
