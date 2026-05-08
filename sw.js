9cbb2d09ee882cb85586fb61689e1108a1b96dfc
aolmio <mchan448@gmail.com>
2026-05-08 23:47:14 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
-const CACHE_NAME = 'gold-inv-v1';
-const assets = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'gold-pro-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
