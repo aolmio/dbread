ddf0ecba741ffa7798fcbd9f8bbaab4137f00389
aolmio <mchan448@gmail.com>
2026-05-09 11:30:14 +0630

    Refactor service worker install and fetch events

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE_NAME = 'gold-v4';
-const assets = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
